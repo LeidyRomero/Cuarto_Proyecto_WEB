995acd8891900a41dd2ebf1887fffd1a7fd25977
Juliana Prieto <jprietoarcila@gmail.com>
2019-11-08 00:07:35 -0500

    Login

--- car-guide-back/car-guide-front/src/Components/Users/Login.js
+++ car-guide-back/car-guide-front/src/Components/Users/Login.js
@@ -25,7 +25,7 @@ class Login extends React.Component {
     const { error } = this.state;
 
     return (
-        <div>
+        <div className="login-background">
         <Container>
           <Row>
             <Col lg="4" md="4" sm="12">
@@ -33,7 +33,7 @@ class Login extends React.Component {
             </Col>
             <Col lg="4" md="4" sm="12">
               <div className="login-wrap">
-                <h1 className="text-center">Ingresar</h1>
+                <h1 className="login-title">Ingresar</h1>
                 <div className="modal-body">
                   {error.length > 0 ? (
                     <div className="alert alert-danger fade in">{error}</div>
@@ -46,7 +46,7 @@ class Login extends React.Component {
                     onSubmit={this.handleSubmit}
                   >
                     <div className="form-group">
-                      <label>Correo electronico: <input
+                      <label>Correo electronico <input
                         type="email"
                         id="login-email"
                         className="form-control input-lg"
@@ -54,7 +54,7 @@ class Login extends React.Component {
                         required/></label>
                     </div>
                     <div className="form-group">
-                      <label>Contraseña: <input
+                      <label>Contraseña <input
                         type="password"
                         id="login-password"
                         className="form-control input-lg"
@@ -65,14 +65,14 @@ class Login extends React.Component {
                       <input
                         type="submit"
                         id="login-button"
-                        className="btn btn-primary btn-lg btn-block"
+                        className="login-form-button"
                         value="Ingresar"
                       />
                     </div>
                     <div className="form-group text-center">
                       <p className="text-center">
                         ¿No tienes una cuenta? Regístrate{" "}
-                        <Link to="/signup">aquí</Link>
+                        <Link to="/signup" id="form-link">aquí</Link>
                       </p>
                     </div>
                   </form>
